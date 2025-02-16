d901b721a4bf5b191ff7a63fe97832a3243a88d5
ChimarokeAmadi <chirokeamadi@gmail.com>
2025-02-17 00:19:49 +0100

    last commit

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,6 +1,5 @@
 import Hero from "@/components/Hero/Hero";
 import Navbar from "@/components/Navbar/Navbar";
-import SwipeComponent from "@/components/Nawa/Nawa";
 
 export default function Home() {
 	return (
