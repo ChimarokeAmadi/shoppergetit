cf41f032bc2bd16e55033fda0df5b87ead543199
ChimarokeAmadi <chirokeamadi@gmail.com>
2025-02-17 08:19:45 +0100

    fixed nav link

--- src/components/Navbar/Navbar.tsx
+++ src/components/Navbar/Navbar.tsx
@@ -1,36 +1,70 @@
+"use client";
 import Image from "next/image";
+import Link from "next/link";
+import { useState } from "react";
+
 export default function Navbar() {
+	const [activeLink, setActiveLink] = useState<string | null>(null);
+
+	const handleActive = (link: string) => {
+		setActiveLink(link);
+	};
+
 	return (
-		<nav className='container bg-white  py-3 lg:py-[5px] font-[comfortaa]'>
-			<div className=' lg:flex justify-end w-full hidden'>
+		<nav className='container bg-white py-3 lg:py-[5px] font-[comfortaa]'>
+			{/* Desktop Navigation */}
+			<div className='lg:flex justify-end w-full hidden'>
 				<div className='flex items-center mr-[297px]'>
-					<div className=' mr-[9px]'>
+					<div className='mr-[9px]'>
 						<Image height={16} width={12} src='/Vector.png' alt='' />
 					</div>
 					<p className=''>Abuja</p>
 				</div>
 
 				<div className='flex items-center'>
-					<p className='mr-[31px]'>Store</p>
-					<p className='mr-[46px]'>login</p>
+					<Link
+						href={"#"}
+						onClick={() => handleActive("store")}
+						className={`mr-[31px] ${
+							activeLink === "store"
+								? "bg-gradient-to-br from-[#C67250] to-[#9B5B9A] bg-clip-text text-transparent relative after:content-[''] after:absolute after:left-0 after:bottom-[-24px] after:w-full after:h-[1px] after:bg-gradient-to-br after:from-[#C67250] after:to-[#9B5B9A]"
+								: "text-black"
+						}`}>
+						Store
+					</Link>
+					<Link
+						href={"#"}
+						onClick={() => handleActive("login")}
+						className={`mr-[46px] ${
+							activeLink === "login"
+								? "bg-gradient-to-br from-[#C67250] to-[#9B5B9A] bg-clip-text text-transparent border-b relative after:content-[''] after:absolute after:left-0 after:bottom-[-24px] after:w-full after:h-[1px] after:bg-gradient-to-br after:from-[#C67250] after:to-[#9B5B9A]"
+								: "text-black"
+						}`}>
+						Login
+					</Link>
 					<button className='bg-[#B63B56] px-[13px] py-[18px] text-white rounded-[8px]'>
 						Become a shopper
 					</button>
 				</div>
 			</div>
-			<div className='flex items-center gap-[14px] justify-end  lg:hidden'>
-				<div className=''>
-					<Image
-						width={34.17}
-						height={34.17}
-						src={"/user.png"}
-						alt=''
-						className='size-[41px]'
-					/>
-				</div>
-				<div className=''>
-					<Image width={21.33} height={16} src={"/menu.png"} alt='' />
-				</div>
+
+			{/* Mobile Navigation */}
+			<div className='flex items-center gap-[14px] justify-end lg:hidden'>
+				<a
+					href='#'
+					className="relative text-black font-bold text-lg pb-2
+                 after:content-[''] after:absolute after:left-0 after:bottom-0 after:w-full after:h-[3px] 
+                 after:bg-gradient-to-br after:from-[#C67250] after:to-[#9B5B9A]">
+					Gradient Border Link
+				</a>
+				<Image
+					width={34.17}
+					height={34.17}
+					src={"/user.png"}
+					alt=''
+					className='size-[41px]'
+				/>
+				<Image width={21.33} height={16} src={"/menu.png"} alt='' />
 			</div>
 		</nav>
 	);
